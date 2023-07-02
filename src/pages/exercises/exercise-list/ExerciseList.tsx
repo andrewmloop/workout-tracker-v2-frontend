cbe27d4b47ea6e662f5247e39f6f1390d6aa3444
aml <andrewmloop@gmail.com>
2023-07-02 15:29:43 -0400

    Add ExerciseList page styling

--- src/pages/exercises/exercise-list/ExerciseList.tsx
+++ src/pages/exercises/exercise-list/ExerciseList.tsx
@@ -7,26 +7,24 @@ export default function ExerciseList() {
     <>
       <TopNav showBackButton={false} navText="Exercises" />
       <div className="exercise-list-page page-container">
-        <ul>
-          <ListItem itemName="All Exercises" muscle="all" />
-          <ListItem itemName="Abdominals" muscle="abdominals" />
-          <ListItem itemName="Abductors" muscle="abductors" />
-          <ListItem itemName="Adductors" muscle="adductors" />
-          <ListItem itemName="Biceps" muscle="biceps" />
-          <ListItem itemName="Calves" muscle="calves" />
-          <ListItem itemName="Chest" muscle="chest" />
-          <ListItem itemName="Forearms" muscle="forearms" />
-          <ListItem itemName="Glutes" muscle="glutes" />
-          <ListItem itemName="Hamstrings" muscle="hamstrings" />
-          <ListItem itemName="Lats" muscle="lats" />
-          <ListItem itemName="Lowerback" muscle="lowerback" />
-          <ListItem itemName="Middleback" muscle="middleback" />
-          <ListItem itemName="Neck" muscle="neck" />
-          <ListItem itemName="Quadriceps" muscle="quadriceps" />
-          <ListItem itemName="Shoulders" muscle="shoulders" />
-          <ListItem itemName="Traps" muscle="traps" />
-          <ListItem itemName="Triceps" muscle="triceps" />
-        </ul>
+        <ListItem itemName="All Exercises" muscle="all" />
+        <ListItem itemName="Abdominals" muscle="abdominals" />
+        <ListItem itemName="Abductors" muscle="abductors" />
+        <ListItem itemName="Adductors" muscle="adductors" />
+        <ListItem itemName="Biceps" muscle="biceps" />
+        <ListItem itemName="Calves" muscle="calves" />
+        <ListItem itemName="Chest" muscle="chest" />
+        <ListItem itemName="Forearms" muscle="forearms" />
+        <ListItem itemName="Glutes" muscle="glutes" />
+        <ListItem itemName="Hamstrings" muscle="hamstrings" />
+        <ListItem itemName="Lats" muscle="lats" />
+        <ListItem itemName="Lowerback" muscle="lowerback" />
+        <ListItem itemName="Middleback" muscle="middleback" />
+        <ListItem itemName="Neck" muscle="neck" />
+        <ListItem itemName="Quadriceps" muscle="quadriceps" />
+        <ListItem itemName="Shoulders" muscle="shoulders" />
+        <ListItem itemName="Traps" muscle="traps" />
+        <ListItem itemName="Triceps" muscle="triceps" />
       </div>
     </>
   );
@@ -38,7 +36,7 @@ function ListItem(props: ListItemProps) {
   const { itemName, muscle } = props;
   return (
     <Link to={"/exercises/" + muscle} className="list-item-container">
-      <p className="item-text">{itemName}</p>
+      <p>{itemName}</p>
       <div className="nav-arrow">--&gt;</div>
     </Link>
   );
