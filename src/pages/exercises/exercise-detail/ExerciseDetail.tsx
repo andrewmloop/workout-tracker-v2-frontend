9255df8ed2b538b13a0ab8da85f52f0ceacf58e0
aml <andrewmloop@gmail.com>
2023-07-02 20:37:47 -0400

    Add ExerciseDetail page styling

--- src/pages/exercises/exercise-detail/ExerciseDetail.tsx
+++ src/pages/exercises/exercise-detail/ExerciseDetail.tsx
@@ -9,31 +9,70 @@ export default function ExerciseDetail() {
 
   return (
     <>
-      <TopNav showBackButton={true} navText="" />
+      <TopNav showBackButton={true} navText={exercise.name} />
       <div className="exercise-detail-page page-container">
-        <p className="name">{exercise.name}</p>
-        {exercise.primaryMuscles.map((muscle, index) => (
-          <p key={index} className="muscles">
-            {muscle}
-          </p>
-        ))}
-        {exercise.secondaryMuscles &&
-          exercise.secondaryMuscles.map((muscle, index) => (
-            <p key={index} className="muscles">
-              {muscle}
-            </p>
-          ))}
-        <p className="category">{exercise.category}</p>
-        <p className="level">{exercise.level}</p>
-        {exercise.equipment && (
-          <p className="equipment">{exercise.equipment}</p>
+        <div className="exercise-info-container">
+          <p className="exercise-info-header">Primary Muscles</p>
+          <div className="muscle-container">
+            {exercise.primaryMuscles.map((muscle, index) => (
+              <p key={index} className="muscle-tag">
+                {muscle.slice(0, 1).toUpperCase() + muscle.slice(1)}
+              </p>
+            ))}
+          </div>
+        </div>
+        {exercise.secondaryMuscles.length > 0 && (
+          <div className="exercise-info-container">
+            <p className="exercise-info-header">Secondary Muscles</p>
+            <div className="muscle-container">
+              {exercise.secondaryMuscles.map((muscle, index) => (
+                <p key={index} className="muscle-tag">
+                  {muscle.slice(0, 1).toUpperCase() + muscle.slice(1)}
+                </p>
+              ))}
+            </div>
+          </div>
         )}
-        {exercise.instructions &&
-          exercise.instructions.map((instruction, index) => (
-            <p key={index} className="instructions">
-              {instruction}
+        <div className="category-level-equipment-container">
+          <div className="level-container">
+            <p className="exercise-info-header">Level</p>
+            <p className="level">
+              {
+                { adv: "Advanced", int: "Intermediate", beg: "Beginner" }[
+                  exercise.level
+                ]
+              }
             </p>
-          ))}
+          </div>
+          <div className="category-container">
+            <p className="exercise-info-header">Category</p>
+            <p className="level">
+              {exercise.category.slice(0, 1).toUpperCase() +
+                exercise.category.slice(1)}
+            </p>
+          </div>
+          {exercise.equipment && (
+            <div className="equipment-container">
+              <p className="exercise-info-header">Equipment</p>
+              <p className="level">
+                {exercise.equipment.slice(0, 1).toUpperCase() +
+                  exercise.equipment.slice(1)}
+              </p>
+            </div>
+          )}
+        </div>
+        <div className="instructions-container">
+          <p className="exercise-info-header">Instructions</p>
+          <ol className="instructions-list">
+            {exercise.instructions.length > 0
+              ? exercise.instructions.map((instruction, index) => (
+                  <li key={index} className="instructions">
+                    {instruction}
+                  </li>
+                ))
+              : "There are no instructions for this exercise."}
+          </ol>
+        </div>
       </div>
     </>
   );
