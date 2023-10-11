1d7c76612c55fb2ea732a298f690abca13e367aa
aml <andrewmloop@gmail.com>
2023-10-11 15:27:23 -0400

    Break rest timer into smaller components

--- src/components/rest-timer/RestTimer.tsx
+++ src/components/rest-timer/RestTimer.tsx
@@ -1,16 +1,14 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useTimerContext } from "../../context/timer-context";
 import "./RestTimer.css";
 
+/**
+ * Rest timer that uses the TimerContext as a global
+ * store to access the remaining rest time throughout
+ * the app.
+ */
 export default function RestTimer() {
-  const {
-    timerStore,
-    setRestTime,
-    setStartTime,
-    setTimeLeft,
-    toggleTimer,
-    endTimer,
-  } = useTimerContext();
+  const { timerStore, setTimeLeft, endTimer } = useTimerContext();
 
   useEffect(() => {
     const now = new Date().getTime();
@@ -40,15 +38,56 @@ export default function RestTimer() {
     }
   }, [timerStore.startTime, timerStore.timeLeft]);
 
-  const formatTime = (secs: number) => {
-    let time = new Date(0);
-    time.setSeconds(secs);
-    return time.toISOString().slice(14, 19);
-  };
+  return (
+    <div className="rest-timer-container">
+      <RestTimerSelect />
+      <RestTimerButton />
+    </div>
+  );
+}
+
+/**
+ * The dropdown component that controls how long the rest timer
+ * should be.
+ */
+function RestTimerSelect() {
+  const { timerStore, setRestTime, endTimer } = useTimerContext();
 
   const handleSelect = (value: number) => {
     setRestTime(value);
     endTimer();
+  };
+
+  return (
+    <div className="rest-timer-dropdown-container">
+      <label>Rest Time</label>
+      <select
+        value={timerStore.restTime}
+        onChange={(e) => handleSelect(+e.target.value)}
+      >
+        <option value={30}>30 sec</option>
+        <option value={60}>1 min</option>
+        <option value={90}>90 sec</option>
+        <option value={120}>2 min</option>
+        <option value={150}>150 sec</option>
+        <option value={180}>3 min</option>
+      </select>
+    </div>
+  );
+}
+
+/**
+ * The button component that starts, stops, and displays how much time
+ * remains on the rest timer.
+ */
+function RestTimerButton() {
+  const { timerStore, setStartTime, setTimeLeft, toggleTimer } =
+    useTimerContext();
+
+  const formatTime = (secs: number) => {
+    let time = new Date(0);
+    time.setSeconds(secs);
+    return time.toISOString().slice(14, 19);
   };
 
   const handleStart = () => {
@@ -58,26 +97,10 @@ export default function RestTimer() {
   };
 
   return (
-    <div className="rest-timer-container">
-      <div className="rest-timer-dropdown-container">
-        <label>Rest Time</label>
-        <select
-          value={timerStore.restTime}
-          onChange={(e) => handleSelect(+e.target.value)}
-        >
-          <option value={30}>30 sec</option>
-          <option value={60}>1 min</option>
-          <option value={90}>90 sec</option>
-          <option value={120}>2 min</option>
-          <option value={150}>150 sec</option>
-          <option value={180}>3 min</option>
-        </select>
-      </div>
-      <div className="rest-timer-button-container">
-        <button onClick={handleStart}>
-          {timerStore.startTimer ? formatTime(timerStore.timeLeft) : "Start"}
-        </button>
-      </div>
+    <div className="rest-timer-button-container">
+      <button onClick={handleStart}>
+        {timerStore.startTimer ? formatTime(timerStore.timeLeft) : "Start"}
+      </button>
     </div>
   );
 }
